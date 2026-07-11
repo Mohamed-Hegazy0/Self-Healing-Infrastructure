a11513c4d24b0cf781e5178cc927d69b65411975
Ubuntu <ubuntu@ip-10-0-101-161.ec2.internal>
2026-07-11 02:41:29 +0000

    Fix syntax error in console.log

--- app/server.js
+++ app/server.js
@@ -106,7 +106,7 @@ app.use((req, res) => {
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(\`Server running on port \${PORT}\`);
+  console.log(`Server running on port \${PORT}\`);
 });
 
 module.exports = app;
